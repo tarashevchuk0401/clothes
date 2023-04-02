3cd0265521a47ed624c5b820dedf03e2555809ef
tarashevchuk0401 <tarashevchuk0401@gmail.com>
2023-04-02 17:31:42 +0200

    test

---
+++ src/app/cloth/cloth.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { Cloth } from '../shared/models/Cloth';
+import { ClothesServiceService } from '../services/clothes-service.service';
+
+@Component({
+  selector: 'app-cloth',
+  templateUrl: './cloth.component.html',
+  styleUrls: ['./cloth.component.css']
+})
+export class ClothComponent implements OnInit{
+
+  clothes: Cloth[] = [];
+  res: Cloth[] = [];
+
+  constructor(private clothesService: ClothesServiceService){}
+
+  ngOnInit(): void {
+    this.clothesService.getAll().subscribe((d: any) => {
+      this.clothes = d;
+    })
+
+  
+
+  }
+
+
+}
