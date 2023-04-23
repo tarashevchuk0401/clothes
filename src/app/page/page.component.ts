ec62b91ac78e655fa23ed7a8fbec13d9d7998284
Taras Shevchuk <tarashevchuk0401@gmail.com>
2023-04-23 18:36:10 +0200

    unsubscribed

--- src/app/page/page.component.ts
+++ src/app/page/page.component.ts
@@ -1,24 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ClothesServiceService } from '../services/clothes-service.service';
 import { Cloth } from '../shared/models/Cloth';
 import { ActivatedRoute } from '@angular/router';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-page',
   templateUrl: './page.component.html',
   styleUrls: ['./page.component.css']
 })
-export class PageComponent implements OnInit {
+export class PageComponent implements OnInit, OnDestroy {
 
   clothes: Cloth[] = [];
+  destroyed$ = new Subject<void>();
 
   constructor(private service: ClothesServiceService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.route.params.subscribe((params) => {
+    this.route.params.pipe(takeUntil(this.destroyed$)).subscribe((params) => {
       if (params['id'])
         this.clothes = this.service.getById(params['id'])
     })
+  }
+
+  ngOnDestroy(): void {
+    this.destroyed$.next();
+    this.destroyed$.complete()
   }
 
 }
